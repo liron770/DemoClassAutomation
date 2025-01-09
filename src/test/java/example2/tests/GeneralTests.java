cae449734d81e2c3101d1f4b1519e309e81b664d
hagai.tregerman <hagai.tregerman@forcepoint.com>
2025-01-09 09:45:49 +0200

    add javadocs

--- src/test/java/example2/tests/GeneralTests.java
+++ src/test/java/example2/tests/GeneralTests.java
@@ -1,6 +1,5 @@
 package example2.tests;
 
-
 import all.utils.GenerateDriverAll;
 import all.utils.JsonUtils;
 import example2.actions.Actions;
@@ -14,24 +13,32 @@ public class GeneralTests {
     WebDriver driver;
     Actions actions;
 
-
+    /**
+     * Sets up the test environment by initializing the WebDriver and Actions.
+     * Reads the URL and browser type from the JSON configuration file.
+     */
     @BeforeSuite
-    public void setUp()  {
+    public void setUp() {
         String URL = JsonUtils.readJsonFromFile("url");
         String BROWSER = JsonUtils.readJsonFromFile("browser");
         driver = GenerateDriverAll.initDriver(BROWSER, URL);
         actions = new Actions(driver);
     }
 
-
-    @Test
+    /**
+     * Test to compare the Chrome CPU value.
+     * Asserts that the comparison is successful.
+     */
+    @Test(description = "Compare Chrome CPU value")
     public void compareChromeCPU() {
         assert actions.compareChromeCpuValue();
     }
 
+    /**
+     * Cleans up the test environment by quitting the WebDriver.
+     */
     @AfterSuite
     public void tearDown() {
         GenerateDriverAll.cleanDriver(driver);
     }
-
 }
